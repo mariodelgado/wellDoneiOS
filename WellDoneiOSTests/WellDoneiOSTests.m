796636b77691b3ea0c750409f522da2ce5a2250d
Rajat Bhargava <rajat.bhargava@gmail.com>
2014-07-09 10:09:09 -0700

    Initial Commit

---
+++ WellDoneiOSTests/WellDoneiOSTests.m
@@ -0,0 +1,34 @@
+//
+//  WellDoneiOSTests.m
+//  WellDoneiOSTests
+//
+//  Created by Bhargava, Rajat on 7/9/14.
+//  Copyright (c) 2014 welldone. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WellDoneiOSTests : XCTestCase
+
+@end
+
+@implementation WellDoneiOSTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
